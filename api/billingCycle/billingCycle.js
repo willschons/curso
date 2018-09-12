355ae6073c40d6888300e95bf6025a5aeafba4e7
William Schons <williamschons13@gmail.com>
2018-09-12 18:34:18 -0300

    71

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -3,7 +3,7 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema ({
     name: { type: String, required:true},
-    value: { type: Number, min: 0, required: true},
+    value: { type: Number, min: 0, required: true}
 
 })
 
--- config/database.js
+++ config/database.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance', {useNewUrlParser: true} )
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
