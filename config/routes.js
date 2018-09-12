574dea193e42833a7c7cb86fd4e2aab06033fca7
William Schons <williamschons13@gmail.com>
2018-09-12 17:35:53 -0300

    60

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,7 @@ module.exports = function(server){
     const router = express.Router()
     server.use('/api', router)
 
+    
     // rotas API
     const billingCycleService = require('../api/billingCycle/billingCycleService')
     billingCycleService.register(router, '/billingCycles')
